106ae7aeda76aa3b67f896085607699d4309cf87
Andhika <Dhika.putratama@gmail.com>
2025-11-06 00:03:43 +0700

    fix: move API URL to env + added .envTemplate

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,1 @@
-// export const API_URL =
-//   process.env.API_URL || "http://localhost:5001/api";
-
-export const API_URL = "http://localhost:5001/api";
+export const API_URL = process.env.EXPO_PUBLIC_PSQL_URL;
